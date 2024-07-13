2114ee60a17f6d7e4cd24eeef86e6c602539efbc
AlohaKuino <127180614+AlohaKuino@users.noreply.github.com>
2024-07-13 15:01:56 +0700

    Update task1.sql

--- cftTest/task1/task1.sql
+++ cftTest/task1/task1.sql
@@ -1,27 +1,27 @@
 CREATE TABLE firstTable(
- 	PersonID integer primary key, 
+    PersonID integer primary key, 
     FirstName varchar(20), 
     LastName varchar(20)
 );
     
 CREATE TABLE secondTable(
-  AddressID integer primary key,
-  PersonID integer, 
-  Region varchar(30), 
-  City varchar(20)
+    AddressID integer primary key,
+    PersonID integer, 
+    Region varchar(30), 
+    City varchar(20)
 );
 
 INSERT INTO firstTable (PersonID, FirstName, LastName)
-	VALUES (1, 'Петр', 'Иванов');
+    VALUES (1, 'Петр', 'Иванов');
 INSERT INTO firstTable (PersonID, FirstName, LastName)
-	VALUES (2, 'Алексей', 'Смирнов');
+    VALUES (2, 'Алексей', 'Смирнов');
 INSERT INTO firstTable (PersonID, FirstName, LastName) 
-	VALUES (3, 'Райан', 'Гослинг');
+    VALUES (3, 'Райан', 'Гослинг');
 
 INSERT INTO secondTable (AddressID, PersonID, Region, City)
-	VALUES (1, 2, 'Новосибирская Область', 'Новосибирск');
+    VALUES (1, 2, 'Новосибирская Область', 'Новосибирск');
 INSERT INTO secondTable (AddressID, PersonID, Region, City)
-	VALUES (2, 3, 'Новосибирская Область', 'Бердск');
+    VALUES (2, 3, 'Новосибирская Область', 'Бердск');
     
 SELECT 
     persons.PersonID,
